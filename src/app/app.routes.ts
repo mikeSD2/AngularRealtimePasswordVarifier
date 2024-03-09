5dc73c0dc526a3c8a3b1c7239e24b77ba79155f8
MikeShadrin <75907087+MikeShadrin@users.noreply.github.com>
2024-03-09 20:34:42 +0200

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { PasswordValidationComponent } from './password-validation/password-validation.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'passwordcheck', component: PasswordValidationComponent },
+  { path: '', redirectTo: '/passwordcheck', pathMatch: 'full' },
+  { path: '**', redirectTo: '/passwordcheck', pathMatch: 'full' },
+];
